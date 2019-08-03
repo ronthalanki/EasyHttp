2b4e92b284f39a82dae1a08994d326070e636f98
Sairanjth Thalanki <sthalanki@berkeley.edu>
2019-08-03 14:59:17 -0700

    Add parameter validation

--- src/main/java/easyhttp/EasyHttp.java
+++ src/main/java/easyhttp/EasyHttp.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.message.BasicNameValuePair;
@@ -24,6 +26,8 @@ public class EasyHttp {
   }
 
   public Response get(final URL url) throws IOException {
+    Objects.requireNonNull(url);
+
     final HttpURLConnection con = (HttpURLConnection) url.openConnection();
     con.setRequestMethod("GET");
 
@@ -33,6 +37,8 @@ public class EasyHttp {
   }
 
   public Response post(final URL url) throws IOException {
+    Objects.requireNonNull(url);
+
     final HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
     final Response response = getResponse(con);
@@ -41,6 +47,9 @@ public class EasyHttp {
   }
 
   public Response post(final URL url, final String payload) throws IOException {
+    Objects.requireNonNull(url);
+    requireJsonNotBlank(payload);
+
     final HttpURLConnection con = (HttpURLConnection) url.openConnection();
     con.setRequestMethod("POST");
     setupPayload(con, payload);
@@ -51,6 +60,8 @@ public class EasyHttp {
   }
 
   public Response put(final URL url) throws IOException {
+    Objects.requireNonNull(url);
+
     final HttpURLConnection con = (HttpURLConnection) url.openConnection();
     con.setRequestMethod("PUT");
 
@@ -60,6 +71,9 @@ public class EasyHttp {
   }
 
   public Response put(final URL url, final String payload) throws IOException {
+    Objects.requireNonNull(url);
+    requireJsonNotBlank(payload);
+
     final HttpURLConnection con = (HttpURLConnection) url.openConnection();
     con.setRequestMethod("PUT");
     setupPayload(con, payload);
@@ -70,6 +84,8 @@ public class EasyHttp {
   }
 
   public Response delete(final URL url) throws IOException {
+    Objects.requireNonNull(url);
+
     final HttpURLConnection con = (HttpURLConnection) url.openConnection();
     con.setRequestMethod("DELETE");
 
@@ -79,6 +95,9 @@ public class EasyHttp {
   }
 
   private void setupPayload(final HttpURLConnection con, final String payload) throws IOException {
+    Objects.requireNonNull(con);
+    requireJsonNotBlank(payload);
+
     con.setRequestProperty("Content-Type", "application/json");
     final OutputStream os = con.getOutputStream();
     byte[] input = payload.getBytes("utf-8");
@@ -86,6 +105,8 @@ public class EasyHttp {
   }
 
   private Response getResponse(final HttpURLConnection con) throws IOException {
+    Objects.requireNonNull(con);
+
     con.setRequestProperty("Accept", "application/json");
     con.setDoOutput(true);
     final BufferedReader bufferedReader =
@@ -101,11 +122,16 @@ public class EasyHttp {
   }
 
   public URL url(final String path) throws URISyntaxException, MalformedURLException {
+    requirePathNotBlank(path);
+
     return url(path, Collections.emptyMap());
   }
 
   public URL url(final String path, final Map<String, Object> queryParams)
       throws URISyntaxException, MalformedURLException {
+    requirePathNotBlank(path);
+    Objects.requireNonNull(queryParams);
+
     final List<NameValuePair> nvps = new ArrayList<>();
     for (Map.Entry<String, Object> entry : queryParams.entrySet()) {
       final BasicNameValuePair nameValuePair =
@@ -114,5 +140,17 @@ public class EasyHttp {
     }
 
     return new URIBuilder(baseUrl).setPath(path).setParameters(nvps).build().toURL();
+  }
+
+  public static void requireJsonNotBlank(final String str) {
+    if (StringUtils.isBlank(str)) {
+      throw new IllegalArgumentException("JSON is empty, null, or blank");
+    }
+  }
+
+  public static void requirePathNotBlank(final String str) {
+    if (StringUtils.isBlank(str)) {
+      throw new IllegalArgumentException("Path is empty, null, or blank");
+    }
   }
 }
