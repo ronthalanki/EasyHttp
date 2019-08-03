bea2353d4e7cf6157f46e1bdf185644014347514
Sairanjth Thalanki <sthalanki@berkeley.edu>
2019-08-03 14:31:48 -0700

    Add URL methods and style

--- src/main/java/easyhttp/EasyHttp.java
+++ src/main/java/easyhttp/EasyHttp.java
@@ -1,45 +1,60 @@
 package easyhttp;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.message.BasicNameValuePair;
 
 public class EasyHttp {
-    private final String baseUrl;
+  private final String baseUrl;
 
-    public EasyHttp(String baseUrl) {
-        this.baseUrl = baseUrl;
+  public EasyHttp(String baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  public Response get(final URL url) {
+    return null;
+  }
+
+  public Response post(final URL url) {
+    return null;
+  }
+
+  public Response post(final URL url, final String payload) {
+    return null;
+  }
+
+  public Response put(final URL url) {
+    return null;
+  }
+
+  public Response put(final URL url, final String payload) {
+    return null;
+  }
+
+  public Response delete(final URL url) {
+    return null;
+  }
+
+  public URL url(final String path) throws URISyntaxException, MalformedURLException {
+    return url(path, Collections.emptyMap());
+  }
+
+  public URL url(final String path, final Map<String, Object> queryParams)
+      throws URISyntaxException, MalformedURLException {
+    final List<NameValuePair> nvps = new ArrayList<>();
+    for (Map.Entry<String, Object> entry : queryParams.entrySet()) {
+      final BasicNameValuePair nameValuePair =
+          new BasicNameValuePair(entry.getKey(), String.valueOf(entry.getValue()));
+      nvps.add(nameValuePair);
     }
 
-    public Response get(final URL url) {
-        return null;
-    }
-
-    public Response post(final URL url) {
-        return null;
-    }
-
-    public Response post(final URL url, final String payload) {
-        return null;
-    }
-
-    public Response put(final URL url) {
-        return null;
-    }
-
-    public Response put(final URL url, final String payload) {
-        return null;
-    }
-
-    public Response delete(final URL url) {
-        return null;
-    }
-
-    public URL url(final String path) {
-        return null;
-    }
-
-    public URL url(final String path, final Map<String, Object> queryParams) {
-        return null;
-    }
+    return new URIBuilder(baseUrl).setPath(path).setParameters(nvps).build().toURL();
+  }
 }
-
--- src/main/java/easyhttp/Response.java
+++ src/main/java/easyhttp/Response.java
@@ -1,19 +1,19 @@
 package easyhttp;
 
 public class Response {
-    private final Integer responseCode;
-    private final String json;
+  private final Integer responseCode;
+  private final String json;
 
-    public Response(final Integer responseCode, final String json) {
-        this.responseCode = responseCode;
-        this.json = json;
-    }
+  public Response(final Integer responseCode, final String json) {
+    this.responseCode = responseCode;
+    this.json = json;
+  }
 
-    public Integer getResponseCode() {
-        return responseCode;
-    }
+  public Integer getResponseCode() {
+    return responseCode;
+  }
 
-    public String getJson() {
-        return json;
-    }
+  public String getJson() {
+    return json;
+  }
 }
