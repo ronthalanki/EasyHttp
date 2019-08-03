edddf980e009174f6d2e781bf081a6382b0cdeff
Sairanjth Thalanki <sthalanki@berkeley.edu>
2019-08-03 14:13:54 -0700

    Add method stubs

---
+++ src/main/java/easyhttp/EasyHttp.java
@@ -0,0 +1,45 @@
+package easyhttp;
+
+import java.net.URL;
+import java.util.Map;
+
+public class EasyHttp {
+    private final String baseUrl;
+
+    public EasyHttp(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public Response get(final URL url) {
+        return null;
+    }
+
+    public Response post(final URL url) {
+        return null;
+    }
+
+    public Response post(final URL url, final String payload) {
+        return null;
+    }
+
+    public Response put(final URL url) {
+        return null;
+    }
+
+    public Response put(final URL url, final String payload) {
+        return null;
+    }
+
+    public Response delete(final URL url) {
+        return null;
+    }
+
+    public URL url(final String path) {
+        return null;
+    }
+
+    public URL url(final String path, final Map<String, Object> queryParams) {
+        return null;
+    }
+}
+
