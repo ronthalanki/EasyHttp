bf7a8daf3bb4e1a5214368c4961542689a1a4171
Sairanjth Thalanki <sthalanki@berkeley.edu>
2019-08-03 14:10:31 -0700

    Add Response class

---
+++ src/main/java/easyhttp/Response.java
@@ -0,0 +1,19 @@
+package easyhttp;
+
+public class Response {
+    private final Integer responseCode;
+    private final String json;
+
+    public Response(final Integer responseCode, final String json) {
+        this.responseCode = responseCode;
+        this.json = json;
+    }
+
+    public Integer getResponseCode() {
+        return responseCode;
+    }
+
+    public String getJson() {
+        return json;
+    }
+}
