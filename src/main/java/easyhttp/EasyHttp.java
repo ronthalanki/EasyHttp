ed78b21dc1cc3d516b4c66995ae5101d8fdc0c98
Sairanjth Thalanki <sthalanki@berkeley.edu>
2019-08-03 14:50:42 -0700

    Implement API methods

--- src/main/java/easyhttp/EasyHttp.java
+++ src/main/java/easyhttp/EasyHttp.java
@@ -1,5 +1,10 @@
 package easyhttp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -18,28 +23,81 @@ public class EasyHttp {
     this.baseUrl = baseUrl;
   }
 
-  public Response get(final URL url) {
-    return null;
+  public Response get(final URL url) throws IOException {
+    final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+
+    final Response response = getResponse(con);
+    con.disconnect();
+    return response;
   }
 
-  public Response post(final URL url) {
-    return null;
+  public Response post(final URL url) throws IOException {
+    final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+    final Response response = getResponse(con);
+    con.disconnect();
+    return response;
   }
 
-  public Response post(final URL url, final String payload) {
-    return null;
+  public Response post(final URL url, final String payload) throws IOException {
+    final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("POST");
+    setupPayload(con, payload);
+
+    final Response response = getResponse(con);
+    con.disconnect();
+    return response;
   }
 
-  public Response put(final URL url) {
-    return null;
+  public Response put(final URL url) throws IOException {
+    final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("PUT");
+
+    final Response response = getResponse(con);
+    con.disconnect();
+    return response;
   }
 
-  public Response put(final URL url, final String payload) {
-    return null;
+  public Response put(final URL url, final String payload) throws IOException {
+    final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("PUT");
+    setupPayload(con, payload);
+
+    final Response response = getResponse(con);
+    con.disconnect();
+    return response;
   }
 
-  public Response delete(final URL url) {
-    return null;
+  public Response delete(final URL url) throws IOException {
+    final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("DELETE");
+
+    final Response response = getResponse(con);
+    con.disconnect();
+    return response;
+  }
+
+  private void setupPayload(final HttpURLConnection con, final String payload) throws IOException {
+    con.setRequestProperty("Content-Type", "application/json");
+    final OutputStream os = con.getOutputStream();
+    byte[] input = payload.getBytes("utf-8");
+    os.write(input, 0, input.length);
+  }
+
+  private Response getResponse(final HttpURLConnection con) throws IOException {
+    con.setRequestProperty("Accept", "application/json");
+    con.setDoOutput(true);
+    final BufferedReader bufferedReader =
+        new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
+
+    final StringBuilder response = new StringBuilder();
+    String responseLine = null;
+    while ((responseLine = bufferedReader.readLine()) != null) {
+      response.append(responseLine.trim());
+    }
+
+    return new Response(con.getResponseCode(), response.toString());
   }
 
   public URL url(final String path) throws URISyntaxException, MalformedURLException {
